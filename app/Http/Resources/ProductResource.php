816a6197132977c532a436ffc9ce7a247d0c5c95
sabreenMohammed107 <sabreenm312@gmail.com>
2023-10-08 16:00:10 +0200

    test 1

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -29,8 +29,8 @@ class ProductResource extends JsonResource
             "sizes"=> SizeResource::collection($this->sizes),
             'review'=>ReviewResource::collection($this->review),
             "details"=> DetailsResource::collection($this->details),
-            "color"=> ColorResource::collection($this->color->toArray()),
-            "images"=> ProImageResource::collection($this->images->toArray()),
+            "color"=> ColorResource::collection($this->color)->implode('-'),
+            "images"=> ProImageResource::collection($this->images)->implode('-'),
 
 
             ];
